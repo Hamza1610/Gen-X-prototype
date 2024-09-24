8249f5506b03ef6eeebe2feacf655bf28e7a1f0a
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-24 15:15:23 -0700

    delete practice file

--- Typescript/practice_7.js
+++
@@ -1,7 +0,0 @@
-// Function
-var car;
-car = function (spec, model) {
-    if (model === void 0) { model = 'Unknown'; }
-    return { info: model, spec: spec };
-};
-console.log(car({ name: 'Lexus', color: 'Silver' }));
