ad79657cbcdf5c513a1ef2033963766739f42fc0
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-24 15:15:52 -0700

    delete practice file

--- Typescript/practice_7.ts
+++
@@ -1,9 +0,0 @@
-// Function
-
-let car: Function;
-
-car = (spec: object, model: number | string = 'Unknown'): object => {
-    return {info: model, spec: spec} 
-}
-
-console.log(car({name: 'Lexus', color: 'Silver'}));
