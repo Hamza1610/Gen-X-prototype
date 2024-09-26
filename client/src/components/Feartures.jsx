c0080b5b6344e4153bbeeca72da134640cf0ddff
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-25 17:31:15 -0700

    Create Features component

---
+++ client/src/components/Feartures.jsx
@@ -0,0 +1,27 @@
+const Feature  = () => {
+  return (
+    //features
+    <section class="w-75">
+    <div class="container">
+      <h2>Unleash the Power of AI</h2>
+      <div class="features-grid">
+        <div class="feature">
+          <i class="fas fa-microphone"></i> <h3>Speak Your Commands</h3>
+          <p>Effortlessly control your computer with natural language.</p>
+        </div>
+        <div class="feature">
+          <i class="fas fa-code"></i> <h3>Command Across Platforms</h3>
+          <p>Execute complex tasks across diverse operating systems.</p>
+        </div>
+        <div class="feature">
+          <i class="fas fa-brain"></i> <h3>AI-powered Learning</h3>
+          <p>Gen-X adapts to your needs, becoming more efficient over time.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  )
+}
+
+export default Feature;
