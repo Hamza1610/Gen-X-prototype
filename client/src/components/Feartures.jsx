16b5631cbd568c47e3d7238b3bfd5011b5bc34ce
Hamza1610 <muhammadhamza162003@gmail.com>
2024-09-29 19:04:41 -0700

    modify element structure

--- client/src/components/Feartures.jsx
+++ client/src/components/Feartures.jsx
@@ -1,24 +1,25 @@
 const Feature  = () => {
   return (
     //features
-    <section className="features">
-    <div class="container">
+    <section className="section-component">
       <h2>Unleash the Power of AI</h2>
       <div class="features-grid">
         <div class="feature">
+          <img src="" alt="Command" className="feature-img"/>
           <i class="fas fa-microphone"></i> <h3>Speak Your Commands</h3>
           <p>Effortlessly control your computer with natural language.</p>
         </div>
         <div class="feature">
+          <img src="" alt="Tasks" className="feature-img"/>
           <i class="fas fa-code"></i> <h3>Command Across Platforms</h3>
           <p>Execute complex tasks across diverse operating systems.</p>
         </div>
         <div class="feature">
+          <img src="" alt="AI-powerd" className="feature-img"/>
           <i class="fas fa-brain"></i> <h3>AI-powered Learning</h3>
           <p>Gen-X adapts to your needs, becoming more efficient over time.</p>
         </div>
       </div>
-    </div>
   </section>
 
   )
