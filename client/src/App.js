9e83e78a8e1a4467ee30d5ec79bede87dee5d433
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-03 11:14:12 -0700

    add tab views to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,80 @@
-import Footer from './components/footer';
-import SignUp from './components/sign_up';
-import './App.css';
-import Feature from './components/Feartures';
-import Benefits from './components/Benefits';
-import About from './components/About';
+// import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { useState } from 'react';
+import LangingPage from './pages';
+import GetStartedPage from './pages/get_started';
+import { Container, Button, Navbar, Image, ButtonToolbar, Tab, Tabs } from 'react-bootstrap'
+import Settings from './components/settings';
 
 function App() {
-  return (
-    <div className="App">
-      <header class="hero">
-        <div class="container">
-          <h1>Introducing Gen-X</h1>
-          <p>The AI-powered voice shell executor, built for the future.</p>
-          <a href="/" class="btn">Get started</a>
-          <a href="#signup" class="btn secondary">Sign Up</a>
-        </div>
-      </header>
 
-      <section class="sections">
-        <Feature />
-        <Benefits />
-        <About />
-        {/* SignUp */}
-        <SignUp />
-        {/* Sign in */}
-        {/* <SignIn /> */}
-      </section>
-      {/* Footer */}
-      <Footer />
-    </div>
+  const [showTab, setShowTab] = useState({
+    showSpaceTab: false, 
+    showSettingTab: true, 
+  });
+
+
+  const showSettings = () => {
+    setShowTab({
+      showSpaceTab: true, 
+      showSettingTab: false, 
+    })
+    console.log(showTab);
+  }
+
+  const showSpace = () => {
+    setShowTab({
+      showSpaceTab: false, 
+      showSettingTab: true, 
+    })
+
+    console.log(showTab);
+  }
+
+  return (
+    // remove style when done and paste to getstarted
+    <div className="App" style={{
+      height: '100vh'
+    }}>
+
+
+    {/* /* UI version 1 */ }
+    <Navbar style={{width: '100vw', height: '50px'}}>
+      <Navbar.Brand style={{float: 'left', margin: '10px', fontSize: '2em', fontStyle: 'italic'}}>
+        {/* A gif image that suit Gen-X */}
+        Gen-X
+      </Navbar.Brand>
+    </Navbar>
+    <Container>
+      <aside>
+        <Button id='space-btn' onClick={(event) => showSpace(event)}><i className='fas fa-server'>Space</i></Button>
+        <Button id='settings-btn' onClick={(event) => showSettings(event)}><i className='fas fa-wrench'></i>Settings</Button>
+      </aside>
+      <main>
+        <Container className='space-container' hidden={showTab.showSpaceTab} style={{ width: '100vw'}}>
+          <h4 className='tab-label'>Space</h4>
+        </Container>
+        <Container className='settings-container' hidden  style={{width: '100vw', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+          <h4 className='tab-label' style={{ textAlign: 'start'}}>Settings</h4>
+          <Settings />
+        </Container>
+      </main>
+    </Container>
+    <ButtonToolbar className='mobile-menu-tray' hidden>
+      <Button><i className='fas fa-server'></i></Button>
+      <Button><i className='fas fa-wrench'></i></Button>
+    </ButtonToolbar>
+
+    {/* use the App class for settings: styles, animation etc. */}
+    {/* <BrowserRouter>
+    <div className='App'>
+      <Routes>
+        <Route path='/' Component={ LangingPage }  />
+        <Route path='/get-started' Component={ GetStartedPage } />
+      </Routes>
+    </div> 
+    </BrowserRouter>  */}
+
+  </div>
   );
 }
 
