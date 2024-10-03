354104b655453dc6cc722b629afd3b9ba7663544
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-03 11:18:20 -0700

    create index page

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,35 @@
+import Footer from '../components/footer';
+import SignUp from '../components/sign_up';
+import '../styles/App.css';
+import Feature from '../components/Feartures';
+import Benefits from '../components/Benefits';
+import About from '../components/About';
+
+function LangingPage() {
+  return (
+    <div className="App">
+      <header class="hero">
+        <div class="container">
+          <h1>Introducing Gen-X</h1>
+          <p>The AI-powered voice shell executor, built for the future.</p>
+          <a href="/" class="btn">Get started</a>
+          <a href="#signup" class="btn secondary">Sign Up</a>
+        </div>
+      </header>
+
+      <section class="sections">
+        <Feature />
+        <Benefits />
+        <About />
+        {/* SignUp */}
+        <SignUp />
+        {/* Sign in */}
+        {/* <SignIn /> */}
+      </section>
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default LangingPage;
