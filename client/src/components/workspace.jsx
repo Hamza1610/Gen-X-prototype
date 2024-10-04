4e5e225be8a75132cf62d5117c7f33b09887258a
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-04 14:49:43 -0700

    create workspace component

---
+++ client/src/components/workspace.jsx
@@ -0,0 +1,11 @@
+import '../styles/workspace.css';
+
+export default  function Workspace(props) {
+    /* const {} = props */
+
+    return (
+        <div className='workspace-component'>
+            { /*  This is where the workspace compont lies*/}
+        </div>
+    )
+}
