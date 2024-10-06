d66b0086a5100555e5b5cde1cb305e33f99aeeb8
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-06 15:56:26 -0700

    return initial layout content

--- client/src/App.js
+++ client/src/App.js
@@ -10,71 +10,21 @@ import Workspace from './components/workspace';
 
 function App() {
 
-  const [showTab, setShowTab] = useState({
-    showSpaceTab: false, 
-    showSettingTab: true, 
-  });
-
-
-  const showSettings = () => {
-    setShowTab({
-      showSpaceTab: true, 
-      showSettingTab: false, 
-    })
-    console.log(showTab);
-  }
-
-  const showSpace = () => {
-    setShowTab({
-      showSpaceTab: false, 
-      showSettingTab: true, 
-    })
-
-    console.log(showTab);
-  }
-
   return (
     // remove style when done and paste to getstarted
     <div className="App" style={{
       height: '100vh'
     }}>
 
-
-    {/* /* UI version 1 */ }
-    <Navbar style={{width: '100vw', height: '50px', marginBotton: '10px'}}>
-      <Navbar.Brand style={{float: 'left', margin: '10px', fontSize: '2em', fontStyle: 'italic'}}>
-        {/* A gif image that suit Gen-X */}
-        Gen-X
-      </Navbar.Brand>
-    </Navbar>
-    <Container>
-      <aside>
-        <Button id='space-btn' onClick={(event) => showSpace(event)}><i className='fas fa-server'>Space</i></Button>
-        <Button id='settings-btn' onClick={(event) => showSettings(event)}><i className='fas fa-wrench'></i>Settings</Button>
-      </aside>
-      <main>
-        <Container className='space-container' hidden={showTab.showSpaceTab}>
-          <Workspace />
-        </Container>
-        <Container className='settings-container' hidden={showTab.showSettingTab}>
-          <Settings />
-        </Container>
-      </main>
-    </Container>
-    <ButtonToolbar className='mobile-menu-tray' hidden>
-      <Button><i className='fas fa-server'></i></Button>
-      <Button><i className='fas fa-wrench'></i></Button>
-    </ButtonToolbar>
-
-    {/* use the App class for settings: styles, animation etc. */}
-    {/* <BrowserRouter>
+    {/*  use the App class for settings: styles, animation etc. */}
+    <BrowserRouter>
     <div className='App'>
       <Routes>
         <Route path='/' Component={ LangingPage }  />
         <Route path='/get-started' Component={ GetStartedPage } />
       </Routes>
     </div> 
-    </BrowserRouter>  */}
+    </BrowserRouter> 
 
   </div>
   );
