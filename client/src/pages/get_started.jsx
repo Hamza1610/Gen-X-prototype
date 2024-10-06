91a6782c94c302ec46b1afe9100f8e5b1e282388
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-06 15:50:58 -0700

    move workspace layout to get_started page

--- client/src/pages/get_started.jsx
+++ client/src/pages/get_started.jsx
@@ -1,33 +1,69 @@
 import '../styles/App.css';
 // import backgroundImage from 'ai_header.jpeg'
-import { Container, Button, Navbar, Image, ButtonToolbar } from 'react-bootstrap'
+import { useState } from 'react';
+import LangingPage from './pages';
+import GetStartedPage from './pages/get_started';
+import './styles/get_started.css';
+
+import { Container, Button, Navbar, Image, ButtonToolbar, Tab, Tabs } from 'react-bootstrap'
+import Settings from './components/settings';
+import Workspace from './components/workspace';
 
 function GetStartedPage() {
-  return (
-    <div className="App " style={{
-        height: '100vh'
-      }}>
-        <Navbar>
-            <Navbar.Brand>
-                {/* A gif image that suit Gen-X */}
-                <Image src='.gif'/>
-            </Navbar.Brand>
-        </Navbar>
-        <Container>
-            <aside>
-                <Button><i className='fas fa-server'>Space</i></Button>
-                <Button><i className='fas fa-wrench'></i>Settings</Button>
-            </aside>
-            <main>
+  const [showTab, setShowTab] = useState({
+    showSpaceTab: false, 
+    showSettingTab: true, 
+  });
 
-            </main>
+
+  const showSettings = () => {
+    setShowTab({
+      showSpaceTab: true, 
+      showSettingTab: false, 
+    })
+    console.log(showTab);
+  }
+
+  const showSpace = () => {
+    setShowTab({
+      showSpaceTab: false, 
+      showSettingTab: true, 
+    })
+
+    console.log(showTab);
+  }
+
+  return (
+    // remove style when done and paste to getstarted
+    <div className="App" style={{
+      height: '100vh'
+    }}>
+
+    {/* /* UI version 1 */ }
+    <Navbar style={{width: '100vw', height: '50px', marginBotton: '10px'}}>
+      <Navbar.Brand style={{float: 'left', margin: '10px', fontSize: '2em', fontStyle: 'italic'}}>
+        {/* A gif image that suit Gen-X */}
+        Gen-X
+      </Navbar.Brand>
+    </Navbar>
+    <Container>
+      <aside>
+        <Button id='space-btn' onClick={(event) => showSpace(event)}><i className='fas fa-server'>Space</i></Button>
+        <Button id='settings-btn' onClick={(event) => showSettings(event)}><i className='fas fa-wrench'></i>Settings</Button>
+      </aside>
+      <main>
+        <Container className='space-container' hidden={showTab.showSpaceTab}>
+          <Workspace />
         </Container>
-        <ButtonToolbar className='mobile-menu-tray' hidden>
-            <Button><i className='fas fa-server'></i></Button>
-            <Button><i className='fas fa-wrench'></i></Button>
-        </ButtonToolbar>
-    </div>
-  );
-}
+        <Container className='settings-container' hidden={showTab.showSettingTab}>
+          <Settings />
+        </Container>
+      </main>
+    </Container>
+    <ButtonToolbar className='mobile-menu-tray' hidden>
+      <Button><i className='fas fa-server'></i></Button>
+      <Button><i className='fas fa-wrench'></i></Button>
+    </ButtonToolbar>
+  
 
 export default GetStartedPage;
