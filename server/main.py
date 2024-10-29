f2a55c6c8b27711904bcd28099d71ecf34d9bb12
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-29 03:20:40 -0700

    add CORS policy to server

--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, List, Tuple
 import uvicorn
 
 
 app = FastAPI()
+
+origins = ['http://locahhost:3000']
+# Middleware
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials = True, allow_methods= ["*"], allow_headers=["*"])
+
 
 @app.get("/")
 async def root():
