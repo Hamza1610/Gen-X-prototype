3dff4f7d2e87f110c862219447a23ab6739982db
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-30 03:28:23 -0700

    import and mount socket app

--- server/main.py
+++ server/main.py
@@ -3,14 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, List, Tuple
 import uvicorn
-
+from stream_io import socket_app
 
 app = FastAPI()
 
 origins = ['http://locahhost:3000']
 # Middleware
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials = True, allow_methods= ["*"], allow_headers=["*"])
-
+app.mount('/websocket', socket_app)
 
 @app.get("/")
 async def root():
