5dcf5e19044172ac9ef86720cd3c2a016f206b1e
Hamza1610 <muhammadhamza162003@gmail.com>
2024-10-30 03:18:38 -0700

    add websockect server to strream_io

--- server/stream_io.py
+++ server/stream_io.py
@@ -1,6 +1,12 @@
+from fastapi import FastAPI, WebSocket
 
+socket_app = FastAPI()
 
-class StreamIo:
-    pass
+# websocket server
+@socket_app.ws('/websocket')
+async def accept_message(websocket: WebSocket):
+    await websocket.accept()
 
-
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f' Message from {websocket} : {data}')
