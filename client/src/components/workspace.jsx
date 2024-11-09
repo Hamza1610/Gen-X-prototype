ca0f67fb7241c284128052f43c71ba00ed36dfcb
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-09 13:37:16 -0800

    add toolbar to workspace

--- client/src/components/workspace.jsx
+++ client/src/components/workspace.jsx
@@ -1,3 +1,4 @@
+import { Button, ButtonToolbar, Container } from 'react-bootstrap';
 import '../styles/workspace.css';
 
 export default  function Workspace(props) {
@@ -6,7 +7,14 @@ export default  function Workspace(props) {
     return (
         <div className='workspace-component'>
             { /*  Workspace componant can be different type of work, media, research tools. Can be added as a component*/}
-            
+            <ButtonToolbar className='menu-tray'>
+                <Button className='btn-menu'><i className='fas fa-folder-closed'></i>View</Button>
+                <Button className='btn-menu'><i className='fas fa-download'></i>Download</Button>
+                <Button className='btn-menu'><i className='fab fa-searchengine'></i>Search</Button>
+            </ButtonToolbar>
+            <Container className='workspace-content-board'>
+                
+            </Container>
         </div>
     )
 }
