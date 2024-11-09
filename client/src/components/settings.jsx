5a8e0f1e61c8120747d2c58f792f927f610b88a7
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-09 13:36:14 -0800

    modify font setting

--- client/src/components/settings.jsx
+++ client/src/components/settings.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 export default function Settings() {
 
     const [theme, setTheme] = useState(localStorage.getItem('Theme') || 'dark');
-    const [font, setFont] = useState(localStorage.getItem('Font') ||'1em');
+    const [font, setFont] = useState(localStorage.getItem('Font') || '1em');
 
     const saveSettings = () => {
         localStorage.setItem('Theme', theme)
