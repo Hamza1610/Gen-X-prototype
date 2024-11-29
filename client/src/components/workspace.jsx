e44ecaeaac0b3dc1bb4de12dabd0edd5faae064a
Hamza1610 <muhammadhamza162003@gmail.com>
2024-11-28 23:31:47 -0800

    add menu button and container

--- client/src/components/workspace.jsx
+++ client/src/components/workspace.jsx
@@ -3,21 +3,32 @@ import '../styles/workspace.css';
 
 export default  function Workspace(props) {
     /* const {} = props */
+
+    const viewContent = () => {
+        const viewBoard = document.getElementById('view-container')
+    }
     
+
     return (
         <div className='workspace-component'>
-            { /*  Workspace componant can be different type of work, media, research tools. Can be added as a component*/}
+            { /*  Workspace component can be different type of work, media, research tools. Can be added as a component*/}
             <ButtonToolbar className='menu-tray'>
-                <Button className='btn-menu'><i className='fas fa-folder-closed'></i>View</Button>
-                <Button className='btn-menu'><i className='fas fa-download'></i>Download</Button>
-                <Button className='btn-menu'><i className='fab fa-searchengine'></i>Search</Button>
+                <Button className='btn-menu' id='view-btn'><i className='fas fa-folder-closed'></i><span className='menu-option-label'>View</span></Button>
+                <Button className='btn-menu' id='download-btn'><i className='fas fa-download'></i><span className='menu-option-label'>Download</span></Button>
+                <Button className='btn-menu' id='search-btn'><i className='fab fa-searchengine'></i><span className='menu-option-label'>Search</span></Button>
             </ButtonToolbar>
             <Container className='workspace-content-board'>
-                   <Container className='view-container'>
-                        {/* Teme */}
+                   <Container className='view-container' hidden>
+                        View
                    </Container>
-                   <Container className='download-action-container' ></Container>
-                   <Container className='search-action-container' ></Container>
+                   
+                   <Container className='download-action-container' hidden>
+                        Download
+                   </Container>
+
+                   <Container className='search-action-container hidden' hidden>
+                        Search
+                   </Container>
             </Container>
         </div>
     )
