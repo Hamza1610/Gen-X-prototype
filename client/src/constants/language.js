e6dddffa53b710eb0053abb922240220aa3c5f77
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-18 08:51:20 -0800

    add languages constants

---
+++ client/src/constants/language.js
@@ -0,0 +1,9 @@
+// Browser laguages
+export const languages = {
+    English: ["en-US", "en-GB"],
+    Spanish: ["es-ES"],
+    French: ["fr-FR"],
+    German: ["de-DE"],
+    Chinese: ["zh-CN"],
+    Arabic: ["ar-SA"]
+};
