b5fc79095a6eaa574405ac6a15d32ff67322ab57
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-22 10:09:47 -0800

    add style import

--- client/src/components/Feartures.jsx
+++ client/src/components/Feartures.jsx
@@ -1,3 +1,5 @@
+import '../styles/Features.css'
+
 const Feature  = () => {
   return (
     //features
@@ -5,17 +7,17 @@ const Feature  = () => {
       <h2>Unleash the Power of AI</h2>
       <div class="features-grid">
         <div class="feature">
-          <img src="" alt="Command" className="feature-img"/>
+          <img src="DALL·E voice command.webp" alt="Command" className="feature-img"/>
           <i class="fas fa-microphone"></i> <h3>Speak Your Commands</h3>
           <p>Effortlessly control your computer with natural language.</p>
         </div>
         <div class="feature">
-          <img src="" alt="Tasks" className="feature-img"/>
+          <img src="DALL·E croos plaform.webp" alt="Tasks" className="feature-img"/>
           <i class="fas fa-code"></i> <h3>Command Across Platforms</h3>
           <p>Execute complex tasks across diverse operating systems.</p>
         </div>
         <div class="feature">
-          <img src="" alt="AI-powerd" className="feature-img"/>
+          <img src="DALL·E ai powered learning.webp" alt="AI-powerd" className="feature-img"/>
           <i class="fas fa-brain"></i> <h3>AI-powered Learning</h3>
           <p>Gen-X adapts to your needs, becoming more efficient over time.</p>
         </div>
