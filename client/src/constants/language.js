f6a70d51ce2906d319b5d8588e6cba0f511717e4
Hamza1610 <muhammadhamza162003@gmail.com>
2024-12-22 01:08:23 -0800

    add  language contant

--- client/src/constants/language.js
+++ client/src/constants/language.js
@@ -1,9 +1,11 @@
 // Browser laguages
-export const languages = {
-    English: ["en-US", "en-GB"],
-    Spanish: ["es-ES"],
-    French: ["fr-FR"],
-    German: ["de-DE"],
-    Chinese: ["zh-CN"],
-    Arabic: ["ar-SA"]
-};
+export const  languages = [
+    ["English(en-US)", "en-US"],
+    ["English(en-GB)", "en-GB"],
+    ["Spanish", "es-ES"],
+    ["French",  "fr-FR"],
+    ["German", "de-DE"],
+    ["Chinese", "zh-CN"],
+    ["Arabic", "ar-SA"]
+]
+
